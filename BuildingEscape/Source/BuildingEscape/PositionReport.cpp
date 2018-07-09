41b31abae792215a2611377c5c80ff7f47488329
Rodrigo Montes <rjmontes@gmail.com>
2018-07-09 00:11:16 -0400

    Mid seccion 3 clase 68 - Mostrando la posicion de un actor usando GetActorLocation y usando .ToString() para pasarla a la macro UE_LOG como texto (usando un puntero)

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -9,6 +10,7 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+
 
 	// ...
 }
@@ -19,7 +21,11 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	// Aca va mi codigo:
+	FString ObjectName = GetOwner()->GetName();
+	FVector ActorPosVector = GetOwner()->GetActorLocation();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ActorPosVector.ToString());
 	
 }
 
